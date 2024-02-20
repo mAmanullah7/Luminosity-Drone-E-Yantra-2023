878e34cbaf037c49175e9b930f0568d06cc623b2
Mohammad Amanullah <ullahaman072003@gmail.com>
2024-02-20 19:59:36 +0530

    Rotors Swift
    Rotors Swift Script

---
+++ Build/rotors_swift_interface/CMakeFiles/rotors_swift_interface.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/anas/catkin_ws/devel/.private/rotors_swift_interface/lib/rotors_swift_interface/rotors_swift_interface"
+  "/home/anas/catkin_ws/devel/.private/rotors_swift_interface/lib/rotors_swift_interface/rotors_swift_interface.pdb"
+  "CMakeFiles/rotors_swift_interface.dir/src/swift_trans.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rotors_swift_interface.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Build/rotors_swift_interface/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/anas/catkin_ws/src/luminosity_drone/swift_simulator/rotors_swift_interface/include".split(';') if "/home/anas/catkin_ws/src/luminosity_drone/swift_simulator/rotors_swift_interface/include" != "" else []
+PROJECT_CATKIN_DEPENDS = "roscpp;geometry_msgs;mav_msgs;trajectory_msgs;swift_msgs".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "rotors_swift_interface"
+PROJECT_SPACE_DIR = "/home/anas/catkin_ws/devel/.private/rotors_swift_interface"
+PROJECT_VERSION = "0.0.1"
