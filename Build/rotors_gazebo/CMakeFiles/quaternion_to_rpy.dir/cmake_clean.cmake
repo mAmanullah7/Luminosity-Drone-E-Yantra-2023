96e41ee091674cdd74911729619047b8b93e41a6
Mohammad Amanullah <ullahaman072003@gmail.com>
2024-02-20 19:47:05 +0530

    Rotors Description
    Rotors Description files

---
+++ Build/rotors_gazebo/CMakeFiles/quaternion_to_rpy.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/anas/catkin_ws/devel/.private/rotors_gazebo/lib/rotors_gazebo/quaternion_to_rpy"
+  "/home/anas/catkin_ws/devel/.private/rotors_gazebo/lib/rotors_gazebo/quaternion_to_rpy.pdb"
+  "CMakeFiles/quaternion_to_rpy.dir/src/quaternion_to_rpy.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/quaternion_to_rpy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
